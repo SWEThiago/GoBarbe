67f0c205664ff6bc0c76f76f92b2c0cdd855c5b2
Thiago Dev <thiagodevprogramador@gmail.com>
2022-06-13 21:06:39 -0300

    aula 11 criando loader de models

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello Thiago' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Thiago Gonçalves',
+    email: 'thiagodevprogramador@gmail.com',
+    password_hash: '123456789',
+  });
+  return res.json(user);
+});
 
 export default routes;
